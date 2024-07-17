d05d3ea98e788cd7e2c01c78be7e9e5ef65e83e4
Michael Kelley <michaelhkelley@cox.net>
2024-07-17 17:34:24 -0400

    deleting script
    work on functionality later on

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.createElement('button');
-button.textContent = 'Change Color';
-document.body.appendChild(button);
-
-button.addEventListener('click', () => {
-  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  document.querySelector('h1').style.color = randomColor;
-});
-
